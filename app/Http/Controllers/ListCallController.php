e30cb6eda0fb1dc4827ddeb4024d4dc7d31ffb36
AlexeyFr <asfrolov1983@gmail.com>
2024-06-14 21:19:56 +0300

    make ListCall: migration, model, controller

---
+++ app/Http/Controllers/ListCallController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListCall;
+use Illuminate\Http\Request;
+
+class ListCallController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ListCall $listCall)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ListCall $listCall)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ListCall $listCall)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ListCall $listCall)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function calls() {
+        return $this->hasMany('App\Models\ListCall', 'user_id')->orderBy('id');
+    }
+
 }
